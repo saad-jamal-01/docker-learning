44fade509db62615cc816215cc62c20f4133b995
Md. Saad Ibne Jamal <saad.jamal@enosisbd.com>
2026-07-23 13:49:29 +0600

    Add cors to accept the request from any client

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const Users = require('./src/Models/User.js');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 4000;
 
 function generateSecureString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const randomValues = new Uint32Array(length);
-    
-    crypto.getRandomValues(randomValues); 
-    
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters[randomValues[i] % characters.length];
-    }
-    
-    return result;
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const randomValues = new Uint32Array(length);
+
+  crypto.getRandomValues(randomValues);
+
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters[randomValues[i] % characters.length];
+  }
+
+  return result;
 }
 
 app.get('/', (req, res) => {
@@ -25,20 +28,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/user', async (req, res) => {
-    const users = await Users.findAll();
-    return res.json({ users, totalUserCount: users.length });
+  const users = await Users.findAll();
+  return res.json({ users, totalUserCount: users.length });
 });
 
 app.post('/api/user', async (req, res) => {
-    const newUser = await Users.create({
-        firstName: generateSecureString(10),
-        lastName: generateSecureString(6)
-    });
+  const newUser = await Users.create({
+    firstName: generateSecureString(10),
+    lastName: generateSecureString(6),
+  });
 
-    return res.json({ user: newUser });
+  return res.json({ user: newUser });
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
